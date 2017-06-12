b739931e3090e0982ad5e24ded8c81f570924bda
zileyuan <zileyuan@gmail.com>
2017-06-12 12:42:40 +0800

    init project

---
+++ app.js
@@ -0,0 +1,29 @@
+const Koa = require('koa');
+
+const router = require('koa-router')();
+const bodyParser = require('koa-bodyparser');
+const view = require('koa-view');
+const static = require('koa-static');
+
+const controller_default = require('./controllers/default');
+
+const app = new Koa();
+
+app.use(async (ctx, next) => {
+    const start = new Date().getTime();
+    await next();
+    const ms = new Date().getTime() - start;
+    console.log(`${ctx.request.method} ${ctx.request.url}: ${ms}ms`);
+    ctx.response.set('X-Response-Time', `${ms}ms`);
+});
+
+app.use(bodyParser());
+app.use(view(__dirname + '/views'))
+app.use(static(__dirname + '/static'));
+
+controller_default(router);
+app.use(router.routes());
+
+app.listen(3000);
+console.log('app started at port 3000...');
+
---
+++ controllers/default.js
@@ -0,0 +1,23 @@
+module.exports = function (router) {
+    router.get('/', async (ctx, next) => {
+        await ctx.render('index', {
+            title: 'Welcome'
+        });
+    });
+    router.post('/signin', async (ctx, next) => {
+        let email = ctx.request.body.email || '';
+        let password = ctx.request.body.password || '';
+        if (email === 'admin@example.com' && password === '123456') {
+            console.log('signin ok!');
+            await ctx.render('signin-ok', {
+                title: 'Sign In OK',
+                name: 'Mr Node'
+            });
+        } else {
+            console.log('signin failed!');
+            await ctx.render('signin-failed', {
+                title: 'Sign In Failed'
+            });
+        }
+    });
+};
